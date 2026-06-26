6138e3a9d8c6bc08f6878c414029a6cffd18d4ef
Hassan <hassan@gmail.com>
2026-06-26 21:32:50 +0600

    stats added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
+import StatsCard from "@/components/StatsCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
   <Banner/>
+<StatsCard/>
 <Footer/>
     </div>
   );
